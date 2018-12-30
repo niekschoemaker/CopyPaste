d16b69286e2a2c93d194ea10607ccd5a6d8282cf
Misstake <32094562+pelikaanjpeg@users.noreply.github.com>
2018-12-30 02:30:10 +0100

    Versioning,  once again......

--- CopyPaste.cs
+++ CopyPaste.cs
@@ -21,7 +21,7 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Copy Paste", "Reneb & MiRror & Misstake", "4.1.3", ResourceId = 716)]
+    [Info("Copy Paste", "Reneb & MiRror & Misstake", "4.1.4", ResourceId = 716)]
     [Description("Copy and paste buildings to save them or move them")]
 	
     public class CopyPaste : RustPlugin
